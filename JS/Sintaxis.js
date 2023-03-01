8cd14bd920b91ebbd0174f829166fc9e4badf1cc
Emiliano Orcesi <witchofmiracle@gmail.com>
2023-03-01 12:37:05 -0300

    Guardado de datos de CV

---
+++ JS/Sintaxis.js
@@ -0,0 +1,6 @@
+
+var MiVariable;
+let Nombre;
+const IVA = 21;
+//Comentario de una sola línea
+
---
+++ cv_datos.js
@@ -0,0 +1,9 @@
+
+{
+const Nombre = "Zack Richardson";
+const Email = "zack.richardson@example.com";
+const Fecha_nacimiento = "7/2/1998";
+const Número_de_teléfono = "(541) 652-0899";
+const Experiencia_laboral = "Lorem Ipsum";
+const Habilidades = "Lorem Ipsum";
+}
