0d2276c438e63efa4178c1c65514f9713c50f06b
Emiliano-Orcesi <witchofmiracle@gmail.com>
2023-03-01 23:37:23 -0300

    actualizar los commit

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+
+function evento1() {
+    alert('Zack Richardson')
+}
+function evento2() {
+    alert('zack.richardson@example.com')
+}
+function evento3() {
+    alert('7/2/1998')
+}
+function evento4() {
+    alert('(541) 652-0899')
+}
+function evento5() {
+    alert('Lorem Ipsum ')
+}
+function evento6() {
+    alert('Lorem Ipsum ')
+}
+ 
--- cv_datos.js
+++
@@ -1,9 +0,0 @@
-
-{
-const Nombre = "Zack Richardson";
-const Email = "zack.richardson@example.com";
-const Fecha_nacimiento = "7/2/1998";
-const Número_de_teléfono = "(541) 652-0899";
-const Experiencia_laboral = "Lorem Ipsum";
-const Habilidades = "Lorem Ipsum";
-}
